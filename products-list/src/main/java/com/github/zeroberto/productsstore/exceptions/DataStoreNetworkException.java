e9b8f1a25c5cc5a8fa462c528a2828dec052cdbf
Ze Roberto <roberto.jrnj@gmail.com>
2020-08-26 20:08:51 -0300

    Adding network communication exception in datastore

---
+++ products-list/src/main/java/com/github/zeroberto/productsstore/exceptions/DataStoreNetworkException.java
@@ -0,0 +1,8 @@
+package com.github.zeroberto.productsstore.exceptions;
+
+public final class DataStoreNetworkException extends RuntimeException {
+
+  public DataStoreNetworkException(Throwable cause) {
+    super(cause);
+  }
+}
