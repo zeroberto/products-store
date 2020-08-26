28bffc836c745186bc7438ca961cbc34b68b3424
Ze Roberto <roberto.jrnj@gmail.com>
2020-08-26 20:38:48 -0300

    Creation of the grpc client for the discount api

---
+++ products-list/src/main/java/com/github/zeroberto/productsstore/cmd/grpcclient/DiscountGrpcClient.java
@@ -0,0 +1,26 @@
+package com.github.zeroberto.productsstore.cmd.grpcclient;
+
+import com.github.zeroberto.productsstore.discountcalculator.DiscountCalculatorServiceGrpc;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import lombok.Getter;
+
+import static io.grpc.ManagedChannelBuilder.forTarget;
+
+@Getter
+public final class DiscountGrpcClient {
+
+  private final ManagedChannel channel;
+  private final DiscountCalculatorServiceGrpc.DiscountCalculatorServiceBlockingStub blockingStub;
+  private final DiscountCalculatorServiceGrpc.DiscountCalculatorServiceStub asyncStub;
+
+  public DiscountGrpcClient(String host) {
+    this(forTarget(host));
+  }
+
+  public DiscountGrpcClient(ManagedChannelBuilder<?> channelBuilder) {
+    channel = channelBuilder.build();
+    blockingStub = DiscountCalculatorServiceGrpc.newBlockingStub(channel);
+    asyncStub = DiscountCalculatorServiceGrpc.newStub(channel);
+  }
+}
