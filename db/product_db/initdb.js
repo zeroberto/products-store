7dcc1f7d59b12334e9358bf9e846c16c5f276e96
Ze Roberto <roberto.jrnj@gmail.com>
2020-08-30 00:19:29 -0300

    Fixed integer usage in the initial product base script

--- db/product_db/initdb.js
+++ db/product_db/initdb.js
@@ -1,9 +1,9 @@
 let res = [
   db.products.drop(),
   db.products.insertMany([
-    { price_in_cents: 119, title: "Chocolate", description: "Everybody likes!" },
-    { price_in_cents: 1000, title: "Popcorn", description: "The favorite of moviegoers!" },
-    { price_in_cents: 500, title: "Orange juice", description: "Give me a dollar!" },
+    { price_in_cents: NumberInt(119), title: "Chocolate", description: "Everybody likes!" },
+    { price_in_cents: NumberInt(1000), title: "Popcorn", description: "The favorite of moviegoers!" },
+    { price_in_cents: NumberInt(500), title: "Orange juice", description: "Give me a dollar!" },
   ])
 ]
 
