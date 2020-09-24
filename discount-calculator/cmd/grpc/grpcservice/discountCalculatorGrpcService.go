5f1e90513b61be41d02a212f995f5091800a8687
Ze Roberto <roberto.jrnj@gmail.com>
2020-09-23 21:05:00 -0300

    Implementation of the discount calculation service

--- discount-calculator/cmd/grpc/grpcservice/discountCalculatorGrpcService.go
+++ discount-calculator/cmd/grpc/grpcservice/discountCalculatorGrpcService.go
@@ -3,6 +3,8 @@ package grpcservice
 import (
 	"context"
 
+	"github.com/zeroberto/products-store/discount-calculator/datastore"
+	"github.com/zeroberto/products-store/discount-calculator/model"
 	pbd "github.com/zeroberto/products-store/discount-calculator/pb/discountcalculator"
 	"github.com/zeroberto/products-store/discount-calculator/usecase"
 )
@@ -10,9 +12,32 @@ import (
 // DiscountCalculatorGrpcService is responsible for providing communication with the grpc service
 type DiscountCalculatorGrpcService struct {
 	DUC usecase.DiscountUseCase
+	PDS datastore.ProductDataStore
+	UDS datastore.UserDataStore
 }
 
 // CalculateDiscount is responsible for calculating the discount for a product
 func (dcgs *DiscountCalculatorGrpcService) CalculateDiscount(ctx context.Context, dreq *pbd.DiscountRequest) (*pbd.DiscountResponse, error) {
-	return nil, nil
+	p, err := dcgs.PDS.FindByID(dreq.GetProductId())
+	if err != nil {
+		return nil, err
+	}
+
+	u, err := dcgs.UDS.FindByID(dreq.GetUserId())
+	if err != nil {
+		return nil, err
+	}
+
+	d := dcgs.DUC.CalculateDiscount(p, u)
+
+	return toDiscountResponse(p.ID, u.ID, d), nil
+}
+
+func toDiscountResponse(productID string, userID int64, d *model.Discount) *pbd.DiscountResponse {
+	return &pbd.DiscountResponse{
+		ProductId:    productID,
+		UserId:       userID,
+		Pct:          d.Percentage,
+		ValueInCents: d.ValueInCents,
+	}
 }
